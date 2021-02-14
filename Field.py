edbe33543f887fa894de5f704fc4eb5596891c4c
denisfeo <dfeoktistov523@icloud.com>
2021-02-14 12:06:23 +0300

    Structural template update.

---
+++ Field.py
@@ -0,0 +1,12 @@
+class Field:
+    def __init__(self, window):
+        self.window = window
+
+    def handle(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,10 @@
 import pygame
 
 
+from Field import Field
+from Settings import Settings
+
+
 class MainWindow:
     SIZE = WIDTH, HEIGHT = 1000, 600
     FPS = 120
@@ -9,11 +13,39 @@ class MainWindow:
         self.screen = pygame.display.set_mode(MainWindow.SIZE)
         self.time = pygame.time.Clock()
 
+        self.field = Field(self)
+        self.settings = Settings(self)
+
     def show(self):
         self.start_cycle()
 
     def start_cycle(self):
         running = True
+
         while running:
+            for event in pygame.event.get():
+                self.handle(event)
+
             self.time.tick(self.FPS)
+
+            self.update()
+            self.update_screen()
             pygame.display.flip()
+
+    def update_screen(self):
+        self.screen.fill((0, 0, 0))
+
+        self.draw()
+        self.settings.draw()
+        self.field.draw()
+
+    def draw(self):
+        pass
+
+    def handle(self, event):
+        self.field.handle(event)
+        self.settings.handle(event)
+
+    def update(self):
+        self.field.update()
+        self.settings.update()
